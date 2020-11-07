73d785a694e759aa1b6895e52f0b5526a006e9c3
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-06 21:22:09 -0500

    Update secret_number.py

--- secret_number.py
+++ secret_number.py
@@ -5,11 +5,12 @@ from random import randint
 value = randint(0,10)
 print ("random value is: " + str(value))
 # change this later, Mayukha, Mayukha, CHANGEEEEE
-min = -1
-max = 11
+num_tries = 0
 random_num = value
 guess = input("Enter a number between 0 and 10:")
 print("You guessed " + str(guess))
+num_tries = num_tries + 1
+print("Number of tries: " + str(num_tries))
 if int(guess) > random_num:
     print("Oops, your number was too big! Try again:)")
     # add something to continue guessing
@@ -19,9 +20,10 @@ elif int(guess) < random_num:
     # add something to continue guessing
     #mark how many tries it took
 else:
-    print("You got it! Do you want to play again?")
-    #mark how many tries it took
-
+    play_again = input("You got it! Do you want to play again? (yes/no)")
+    if play_again = "yes":
+        
+    print(play_again)
     # if random_num is greater than generated number:
     # print("Oops, your number was too big! Try again:)")
     # elif num is less than generated number:
