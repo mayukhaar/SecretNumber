7c040c4fbbc0be33169ebe824d4e6da772791f09
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2020-11-06 22:43:37 -0500

    Update secret_number.py

--- secret_number.py
+++ secret_number.py
@@ -1,5 +1,6 @@
 print("GUESS THE SECRET NUMBER")
 print("------------------------")
+
 from random import randint
 
 value = randint(0,10)
@@ -7,26 +8,35 @@ print ("random value is: " + str(value))
 # change this later, Mayukha, Mayukha, CHANGEEEEE
 num_tries = 0
 random_num = value
+
+
 guess = input("Enter a number between 0 and 10:")
 print("You guessed " + str(guess))
 num_tries = num_tries + 1
 print("Number of tries: " + str(num_tries))
+
 if int(guess) > random_num:
     print("Oops, your number was too big! Try again:)")
-    # add something to continue guessing
-    #mark how many tries it took
+    guess = input("Enter a number between 0 and 10:")
+    print("You guessed " + str(guess))
+    num_tries = num_tries + 1
+    print("Number of tries: " + str(num_tries))
 elif int(guess) < random_num:
     print("Oops, your number was too small! Try again:)")
-    # add something to continue guessing
-    #mark how many tries it took
+    guess = input("Enter a number between 0 and 10:")
+    print("You guessed " + str(guess))
+    num_tries = num_tries + 1
+    print("Number of tries: " + str(num_tries))
 else:
-    play_again = input("You got it! Do you want to play again? (yes/no)")
-    if play_again = "yes":
-        
-    print(play_again)
-    # if random_num is greater than generated number:
-    # print("Oops, your number was too big! Try again:)")
-    # elif num is less than generated number:
-    # print("Oops, your number was too small... try again:)")
-    # else:
-    # print("You got it! YAY! Do you want to play again?")
+    play_again = input("You got it!")
+    print(num_tries)
+
+    if play_again == "yes":
+        print(play_again)
+
+    else:
+        print("Bye. Have a great day!! :D ")
+
+
+        #figure out how to repeat game
+        #number of tries increase with game
